3922508e5d0227fe2e4b63631d40ad2e7941ab84
sree <rajan.sreeraj@yahoo.com>
2023-04-08 17:09:02 +0530

    add generator option

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,100 @@
+
 import * as vscode from "vscode";
 import { Configuration, OpenAIApi } from "openai";
+
+async function getOpenAIConfig(): Promise<{ apiKey: string; organizationId: string }> {
+  const config = vscode.workspace.getConfiguration("refactorWithAI");
+  let apiKey = config.get<string>("apiKey");
+  let organizationId = config.get<string>("organizationId");
+
+  if (!apiKey || !organizationId) {
+    apiKey = await vscode.window.showInputBox({
+      prompt: "Please enter your OpenAI API key:",
+      ignoreFocusOut: true,
+    });
+    organizationId = await vscode.window.showInputBox({
+      prompt: "Please enter your OpenAI Organization ID:",
+      ignoreFocusOut: true,
+    });
+
+    if (!apiKey || !organizationId) {
+      vscode.window.showErrorMessage(
+        "API key and Organization ID is required to use the Refactor with AI extension."
+      );
+      throw new Error("Invalid OpenAI configuration");
+    }
+
+    // Save the API key to the user's settings
+    await config.update("apiKey", apiKey, vscode.ConfigurationTarget.Global);
+    await config.update(
+      "organizationId",
+      organizationId,
+      vscode.ConfigurationTarget.Global
+    );
+  }
+  return { apiKey, organizationId };
+}
+
+
+export async function refactorWithAISuggestion(
+  document: vscode.TextDocument,
+  range: vscode.Range
+): Promise<void> {
+  try {
+    const selectedText = document.getText(range);
+    const openAIConfig = await getOpenAIConfig();
+    const prompt = `Suggest refactoring for following code:\n\n${selectedText}\n\n.
+    Assume the output returned is being replaced with the selected code passed in the prompt.
+    Also the reasons should be commented out in the code.`;
+
+    const configuration = new Configuration(openAIConfig);
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+    });
+    let refactorSuggestion = "";
+    if (response.data?.choices.length > 0) {
+      const suggestions = response.data?.choices;
+      refactorSuggestion = suggestions[0].message?.content || "";
+
+      const edit = new vscode.WorkspaceEdit();
+      edit.replace(document.uri, range, refactorSuggestion);
+      await vscode.workspace.applyEdit(edit);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function generateWithAI(
+  document: vscode.TextDocument,
+  range: vscode.Range
+): Promise<void> {
+  try {
+    const selectedText = document.getText(range);
+    const openAIConfig = await getOpenAIConfig();
+    const prompt = `${selectedText}`;
+
+    const configuration = new Configuration(openAIConfig);
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+    });
+    let refactorSuggestion = "";
+    if (response.data?.choices.length > 0) {
+      const suggestions = response.data?.choices;
+      refactorSuggestion = suggestions[0].message?.content || "";
+
+      const edit = new vscode.WorkspaceEdit();
+      edit.replace(document.uri, range, `${prompt}\n${refactorSuggestion}`);
+      await vscode.workspace.applyEdit(edit);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export function activate(context: vscode.ExtensionContext) {
   const refactorWithAISuggestionCommand = vscode.commands.registerCommand(
@@ -16,75 +111,21 @@ export function activate(context: vscode.ExtensionContext) {
     }
   );
 
-  context.subscriptions.push(refactorWithAISuggestionCommand);
-}
-
-export function deactivate() {}
-
-export async function refactorWithAISuggestion(
-  document: vscode.TextDocument,
-  range: vscode.Range
-): Promise<void> {
-  try {
-    // Get the selected text from the document using the range
-    const selectedText = document.getText(range);
-    const prompt = `Suggest refactoring for following code:\n\n${selectedText}\n\n. 
-      Also the reasons should be commented out in the code.`;
-
-    // ... Rest of the OpenAI API call and response handling
-    const config = vscode.workspace.getConfiguration("refactorWithAI");
-    let apiKey = config.get<string>("apiKey");
-    let organizationId = config.get<string>("organizationId");
-
-    if (!apiKey || !organizationId) {
-      apiKey = await vscode.window.showInputBox({
-        prompt: "Please enter your OpenAI API key:",
-        ignoreFocusOut: true,
-      });
-
-      organizationId = await vscode.window.showInputBox({
-        prompt: "Please enter your OpenAI Organization ID:",
-        ignoreFocusOut: true,
-      });
-
-      if (!apiKey || !organizationId) {
-        vscode.window.showErrorMessage(
-          "API key and Organization ID is required to use the Refactor with AI extension."
-        );
+  const generateWithAICommand = vscode.commands.registerCommand(
+    "extension.generateWithAI",
+    async () => {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
         return;
       }
 
-      // Save the API key to the user's settings
-      await config.update("apiKey", apiKey, vscode.ConfigurationTarget.Global);
-      await config.update(
-        "organizationId",
-        organizationId,
-        vscode.ConfigurationTarget.Global
-      );
+      const selectedRange = editor.selection;
+
+      await generateWithAI(editor.document, selectedRange);
     }
+  );
 
-    const configuration = new Configuration({
-      organization: organizationId,
-      apiKey: apiKey,
-    });
-
-    const openai = new OpenAIApi(configuration);
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: prompt }],
-    });
-    let refactorSuggestion = "";
-
-    if (response.data?.choices.length > 0) {
-      const suggestions = response.data?.choices;
-      refactorSuggestion = suggestions[0].message?.content || "";
-
-      // Apply the refactoring suggestion to the selected range
-      const edit = new vscode.WorkspaceEdit();
-      edit.replace(document.uri, range, refactorSuggestion);
-      await vscode.workspace.applyEdit(edit);
-    }
-  } catch (error) {
-    console.log(error);
-  }
+  context.subscriptions.push(refactorWithAISuggestionCommand, generateWithAICommand);
 }
+
+export function deactivate() {}
